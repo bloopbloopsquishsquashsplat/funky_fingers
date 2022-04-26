4065ac9ecc5a3b8f8f1b47129a2c34f89daf394a
eswaggy <eswaggy1234@gmail.com>
2022-04-25 23:55:02 -0700

    update

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    //get all sounds in resources folder, make object array size
+    //equal number of mp3 files.
+    //assign each object in array of sounds with filename
+    
+    // Start is called before the first frame update
+    public Sound[] sounds;
+    
+    
+    void Awake()
+    {
+        
+        foreach (Sound s in sounds)
+        {
+           s.source = gameObject.AddComponent<AudioSource>();
+           s.source.clip = s.clip;
+           //
+           s.source.volume = 1;
+           //s.source.volume = s.volume;
+           //s.source.pitch = s.pitch;
+        }
+        
+    }
+
+    // Update is called once per frame
+    public void Play(string nameInput)
+    {
+        foreach(Sound s in sounds)
+        {
+            if(s.name == nameInput)
+            {
+                s.source.time = 10;
+                //s.source.PlayClipAtPoint(s.clip, new Vector3(5, 1, 2));
+                s.source.Play();
+            }
+        }
+    }
+
+    public void Stop()
+    {
+        foreach(Sound s in sounds)
+        {
+            if(s.source.isPlaying)
+            {
+                s.source.Stop();
+            }
+        }
+    }
+    
+}
