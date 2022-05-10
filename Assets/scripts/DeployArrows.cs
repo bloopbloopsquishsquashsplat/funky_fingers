571c8c11a0967e0fe0d906b8de4a604876bbd224
aadri24 <100242347+aadri24@users.noreply.github.com>
2022-05-10 04:00:17 -0700

    DONEE

--- Assets/scripts/DeployArrows.cs
+++ Assets/scripts/DeployArrows.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeployArrows : MonoBehaviour
 {
+    public static DeployArrows instance;
     public List<GameObject> arrows = new List<GameObject>();
     public float xPos;
     public int arrObj = 0;
     public float bpm = 128f;
     public float respawnTime = 2.5f;
     private Vector2 screenBounds;
+    public int currentScore;
+
+    public int scorePerNote = 100;
+    public int scorePerGoodNote = 125;
+    public int scorePerPerfectNote = 150;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
     //public bool gameStart;
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
+
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
         //respawnTime = 60f / bpm;
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         
@@ -32,7 +50,7 @@ public class DeployArrows : MonoBehaviour
         if(arrObj == 3){
             xPos = -32.43f;
         }
-        a.transform.position = new Vector2(xPos, screenBounds.y * -1);
+        a.transform.position = new Vector2(xPos, screenBounds.y -35);
         arrObj = Random.Range(0,4);
             
     }
@@ -45,5 +63,43 @@ public class DeployArrows : MonoBehaviour
             yield return new WaitForSeconds(respawnTime);
             spawn();
         }
+    }
+    public void NoteHit(){
+        Debug.Log("Hit On Time");
+
+        if(currentMultiplier - 1 < multiplierThresholds.Length){
+
+            multiplierTracker++;
+            if(multiplierThresholds[currentMultiplier - 1] <= multiplierTracker){
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+        multiText.text = "Multiplier: x" + currentMultiplier;
+
+        currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Score: " + currentScore;
+    }
+
+    public void NormalHit(){
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void GoodHit(){
+        currentScore += scorePerGoodNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void PerfectHit(){
+        currentScore += scorePerPerfectNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void NoteMissed(){
+        Debug.Log("Note Missed");
+
+        currentMultiplier = 1;
+        multiplierTracker = 0;
     }
 }
--- Assets/scripts/NoteObject.cs
+++ Assets/scripts/NoteObject.cs
@@ -7,6 +7,8 @@ public class NoteObject : MonoBehaviour
     public bool canBePressed;
 
     public KeyCode keyToPress;
+
+    public GameObject okEffect, goodEffect, perfectEffect, missEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +21,21 @@ public class NoteObject : MonoBehaviour
         if(Input.GetKeyDown(keyToPress)){
             if(canBePressed){
                 gameObject.SetActive(false);
+                //DeployArrows.instance.NoteHit();
 
-                //GameManager.instance.NoteHit();
+                if(transform.position.y > 85.3 && transform.position.y < 85.7){
+                    Debug.Log("Perfect");
+                    DeployArrows.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                }else if(transform.position.y > 85 && transform.position.y < 86){
+                    Debug.Log("Good");
+                    DeployArrows.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                }else if(transform.position.y > 84.7 && transform.position.y < 86.3){
+                    Debug.Log("Ok");
+                    DeployArrows.instance.NormalHit();
+                    Instantiate(okEffect, transform.position, okEffect.transform.rotation);
+                }
             }
         }
     }
@@ -38,7 +53,8 @@ public class NoteObject : MonoBehaviour
         {
             canBePressed = false;
 
-            //GameManager.instance.NoteMissed();
+            DeployArrows.instance.NoteMissed();
+            
         }
     }
 }
